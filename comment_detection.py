6dda7908d921c6e2fd48d0374f949809cf90359c
keshan-spec <keshanth.programmer@gmail.com>
2020-02-06 13:36:27 +0530

    Added comments to API Class

--- comment_detection.py
+++ comment_detection.py
@@ -3,7 +3,7 @@ from api import API
 PREDICT_URL = "http://max-toxic-comment-classifier.max.us-south.containers.appdomain.cloud/model/predict"
 api = API(PREDICT_URL)
 headers = api.HEADERS
-param = api.create_param(["Fucking nigger"])
+param = api.create_param(["Toxic"])
 resp = api.post(param, headers)
 pred = api.get_prediction(resp)
 print(pred)
