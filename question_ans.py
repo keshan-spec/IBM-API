9ee5cda32395e10ed6a67bca0557a1fd2e6d885b
keshan-spec <keshanth.programmer@gmail.com>
2020-02-07 23:54:21 +0530

    Added QnA user input

--- question_ans.py
+++ question_ans.py
@@ -17,16 +17,28 @@ paragraphs": [
 '''
 
 
-# Initialize class
-api = API(PREDICT_URL)
-# Headers from the API Class
-headers = api.HEADERS
-# Create parameters from the API Class method
-param = api.create_qna("John lives in Brussels and works for the EU", ["Where does John Live?", "What does John do?"])
-# Send a post request
-resp = api.post(param, headers)
-# Get the predictions from the response of the POST request
-predictions = api.get_answers(resp, show=True)
+def main(context, questions):
+    # Initialize class
+    api = API(PREDICT_URL)
+    # Headers from the API Class
+    headers = api.HEADERS
+    # Create parameters from the API Class method
+    param = api.create_qna(context, questions)
+    # Send a post request
+    resp = api.post(param, headers)
+    # Get the predictions from the response of the POST request
+    predictions = api.get_answers(resp, show=True)
+    print(predictions)
 
-print(predictions)
 
+ct = input("Enter context (Not more than one line :) > ")
+while not ct:
+    ct = input("Enter context (Not more than one line :) > ")
+
+ques = []
+q = input("Ask away (Enter x to stop)> ")
+while q != "x":
+    ques.append(q)
+    q = input("Ask away (Enter x to stop)> ")
+
+main(ct, ques)
