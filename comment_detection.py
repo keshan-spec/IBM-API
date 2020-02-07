12d465e7c05f59deb633cc9edfbe78d9b1203ec1
keshan-spec <keshanth.programmer@gmail.com>
2020-02-07 23:03:27 +0530

    Added methods and custom input

--- comment_detection.py
+++ comment_detection.py
@@ -2,15 +2,35 @@ from api import API
 
 # IBM Toxic classifier model API
 PREDICT_URL = "http://max-toxic-comment-classifier.max.us-south.containers.appdomain.cloud/model/predict"
-# Initialize class
-api = API(PREDICT_URL)
-# Headers from the API Class
-headers = api.HEADERS
-# Create parameters from the API Class method
-param = api.create_param(["That is good", "i wanna kill you", "you are ugly"])
-# Send a post request
-resp = api.post(param, headers)
-# Get the predictions from the response of the POST request
-predictions = api.get_predictions(resp)
-# Get the highest predictions
-highest = api.get_highest_prediction(predictions)
+# Sample input ["That is good", "i wanna kill you", "you are ugly"]
+
+
+def get_text():
+    inp = input("Enter your text > ")
+    return main([inp])
+
+
+def main(data):
+    # Initialize class
+    api = API(PREDICT_URL)
+    # Headers from the API Class
+    headers = api.HEADERS
+    # Create parameters from the API Class method
+    param = api.create_param(data)
+    # Send a post request
+    resp = api.post(param, headers)
+    # Get the predictions from the response of the POST request
+    predictions = api.get_predictions(resp)
+    # Get the highest predictions
+    highest = api.get_highest_prediction(predictions)
+    # print the highest prediction
+    print(highest)
+
+    choice = input("Try again ? (y/n) > ")
+    if choice.lower() in ["y", "yes"]:
+        get_text()
+    else:
+        return 
+
+
+get_text()
